b59b237c6aaecb443f28558b9e265ab807b6d6a0
Saranator <s_sepulv@uncg.edu>
2021-01-16 11:36:42 -0500

    fixed guessinggame

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,17 +2,17 @@
 # File: guessinggame.sh
 
 function guessinggame {
-
+	files="$(ls -1 | wc -l)"
 	response=0
-	while [[ $response -lt 5 ]] || [[ $response -gt 5 ]]
+	while [[ $response -lt $files ]] || [[ $response -gt $files ]]
 	do
 		echo "How many files do you think are in the current directory?"
 		read response
 
-		if [[ $response -gt 5 ]]
+		if [[ $response -gt $files ]]
 		then
 			echo "Too high. Try again!"
-		elif [[ $response -lt 5 ]]
+		elif [[ $response -lt $files ]]
 		then
 			echo "Too low. Try again!"
 		else
@@ -20,3 +20,4 @@ function guessinggame {
 		fi
 	done
 }
+guessinggame
